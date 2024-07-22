16437f1b05b561a46ec895900fcc29f33d8a83d9
Eric <ec6862@gmail.com>
2024-07-21 22:41:12 -0400

    added tailwindcss installation notes

--- final/src/main.jsx
+++ final/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // MAKE SURE TO IMPORT THIS SO THAT TAILWIND INSTALLATION WORKS
 import Layout from './routes/Layout.jsx'
 import CreatePost from './routes/CreatePost.jsx'
 import HomePage from './routes/HomePage.jsx'
